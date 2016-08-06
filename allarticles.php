96ff9d781411c226ecab8ce1ac641a3103b43a14
Ganesh Sawhney <ganeshsawhney@gmail.com>
2016-08-07 00:09:57 +0530

    Basic files

---
+++ allarticles.php
@@ -0,0 +1,21 @@
+﻿<?php
+echo '
+							<div class="col-xs-12 col-sm-15 col-md-15 col-lg-15">
+							<div class="light-pricing">    <!--div class="light-pricing popular"   For Light and Big Menu-->
+							<div class="price">
+							<sup>! </sup>Article - '. $i .'<small align="left">Written by : &emsp;&emsp; '.$writer.'</small>
+							</div>
+							<div class="type">
+									
+										'.$article.'
+							</div>
+							
+							</div>
+							</div>
+
+							
+						</div>
+						</div>
+						</div>
+						</div>
+						</section>';
---
+++ index.php
@@ -0,0 +1,358 @@
+﻿<!DOCTYPE html>
+<html lang="en" class="no-js" >
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<meta name="description" content="" />
+<meta name="author" content="" />
+<!--[if IE]>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<![endif]-->
+<title>The Untamed Pen</title>
+<!-- BOOTSTRAP CORE CSS -->
+<link href="assets/css/bootstrap.css" rel="stylesheet" />
+<!-- ION ICONS STYLES -->
+<link href="assets/css/ionicons.css" rel="stylesheet" />
+<!-- FONT AWESOME ICONS STYLES -->
+<link href="assets/css/font-awesome.css" rel="stylesheet" />
+<!-- FANCYBOX POPUP STYLES -->
+<link href="assets/js/source/jquery.fancybox.css" rel="stylesheet" />
+<!-- STYLES FOR VIEWPORT ANIMATION -->
+<link href="assets/css/animations.min.css" rel="stylesheet" />
+<!-- CUSTOM CSS -->
+<link href="assets/css/style-blue.css" rel="stylesheet" />
+<!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body data-spy="scroll" data-target="#menu-section">
+<!--MENU SECTION START-->
+<div class="navbar navbar-inverse navbar-fixed-top scroll-me" id="menu-section" >
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" href="#">
+The Untamed Pen
+</a>
+</div>
+<div class="navbar-collapse collapse">
+<ul class="nav navbar-nav navbar-right">
+<li><a href="index.html">HOME</a></li>
+<li><a href="#login">LOGIN</a></li>
+<li><a href="#services">ARTICLES</a></li>
+<li><a href="#team">OUR TEAM</a></li>
+<li><a href="#contact">CONTACT US</a></li>
+</ul>
+</div>
+</div>
+</div>
+<!--MENU SECTION END-->
+
+
+
+
+
+<!--HOME SECTION START-->
+<div id="home" >
+<div class="container">
+<div class="row">
+<div class="col-sm-8 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 ">
+<div id="carousel-slider" data-ride="carousel" class="carousel slide  animate-in" data-anim-type="fade-in-up">
+<div class="carousel-inner">
+
+	<div class="item active">
+	<h3>
+	Creativity Indeed !
+	</h3>
+	<p>
+	<font color=#ffffff>
+	Read some mind refreshing articles.</font>
+	</p>
+	</div>
+
+	<div class="item">
+	<h3>
+	Originality
+	</h3>
+	<p>
+	All the stuff here is original and authentic.
+	</p>
+	</div>
+
+</div>
+</div>
+</div>
+</div>
+<div class="row animate-in" data-anim-type="fade-in-up">
+<div class="col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 col-lg-8 col-lg-offset-2 scroll-me">
+<p >
+The Untamed Pen
+</p>
+<div class="social">
+<a href="https://www.facebook.com/theuntamedpen" class="btn button-custom btn-custom-one" ><i class="fa fa-facebook "></i></a>
+<!--a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-twitter"></i></a>
+<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-google-plus "></i></a>
+<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-linkedin "></i></a>
+<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-pinterest "></i></a>
+<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-github "></i></a-->
+</div>
+<a href="#services" class=" btn button-custom btn-custom-two">See Our Work </a>
+</div>
+</div>
+</div>
+</div>
+<!--HOME SECTION END-->
+
+
+
+
+																			<!--Login options start-->
+																			<section id="login" >
+																			<div class="container">
+																			<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+																			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+																			<h3>Login Options</h3>
+																			<hr />
+																			</div>
+																			</div>
+																			<div class="row pad-bottom animate-in" data-anim-type="fade-in-up">
+																			<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
+																			<div class="row db-padding-btm db-attached">
+
+																				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+																				<div class="light-pricing">    <!--div class="light-pricing popular"   For Light and Big Menu-->
+																				<div class="price">
+																				Sign Up
+																				<small>Social Networking option yet to be added</small>
+																				</div>
+																				<div class="type">
+																				Avail features like marking articles read, by signing up
+																				</div>
+																				<ul>
+																				<li><i class="glyphicon glyphicon-time"></i>Limited time free sign-up option</li>
+																				</ul>
+																				<div class="pricing-footer">
+																				<a href="#" class="btn button-custom btn-custom-one">Sign Up</a>
+																				</div>
+																				</div>
+																				</div>
+
+																				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+																				<div class="light-pricing">    <!--div class="light-pricing popular"   For Light and Big Menu-->
+																				<div class="price">
+																				<font color=##b3b3ff>Login</font>
+																				<small>Social Networking option yet to be added</small>
+																				</div>
+																				<div class="type">
+																				Avail features like marking articles read, by Login
+																				</div>
+																				<ul>
+																				<li><i class="glyphicon glyphicon-time"></i>New features being added everyday</li>
+																				</ul>
+																				<div class="pricing-footer">
+																				<a href="login.php" class="btn button-custom btn-custom-one">Login</a>
+																				</div>
+																				</div>
+																				</div>
+
+																			</div>
+																			</div>
+																			</div>
+																			</div>
+																			</section>
+																			<!--Login SECTION END-->
+
+
+
+
+<!--SERVICE SECTION START-->
+<section id="services" >
+<div class="container">
+<div class="row text-center header">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 animate-in" data-anim-type="fade-in-up">
+<h3>Our Articles</h3>
+<hr />
+</div>
+</div>
+<a href="view_articles.php">
+	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+	<div class="services-wrapper">
+	<i class="ion-calendar"></i>
+	<h3>Read Articles</h3>
+	Just Login and view them
+	</div>
+	</div>
+
+	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+	<div class="services-wrapper">
+	<i class="ion-calendar"></i>
+	<h3>Read Specials</h3>
+	Just Login and view them
+	</div>
+	</div>
+</a>
+	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+	<div class="services-wrapper">
+	<i class="ion-calendar"></i>
+	<h3>Mark for future read</h3>
+	Don't have time but like the article.....We have a solution.
+	</div>
+	</div>
+
+	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+	<div class="services-wrapper">
+	<i class="ion-monitor"></i>
+	<h3>Publish your Writings</h3>
+	Spread your creativity
+	</div>
+	</div>
+
+</div>
+</div>
+</section>
+<!--SERVICE SECTION END-->
+
+
+
+														<!--TEAM SECTION START-->
+														<section id="team" >
+														<div class="container">
+														<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+														<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+														<h3>Team Members </h3>
+														<hr />
+														</div>
+														</div>
+														<div class="row animate-in" data-anim-type="fade-in-up">
+
+															<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+															<div class="team-wrapper">
+															<div class="team-inner" style="background-image: url('assets/img/team/1.jpg')" >
+															<a href="https://www.facebook.com/crucified.gaurav?fref=ts" target="_blank" > <i class="fa fa-facebook" ></i></a>
+															</div>
+															<div class="description">
+															<h3> Gaurav Pandey</h3>
+															<h5> <strong>Writer</strong></h5>
+															<p>
+															Not enough space here to introduce.
+															</p>
+															</div>
+															</div>
+															</div>
+														
+															<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+															<div class="team-wrapper">
+															<div class="team-inner" style="background-image: url('assets/img/team/2.jpg')" >
+															<a href="https://www.facebook.com/anshul.suri?fref=ts" target="_blank" > <i class="fa fa-facebook" ></i></a>
+															</div>
+															<div class="description">
+															<h3> Anshul Suri</h3>
+															<h5> <strong> Writer </strong></h5>
+															<p>
+															Prefers to write then to talk.
+															</p>
+															</div>
+															</div>
+															</div>
+															
+															<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+															<div class="team-wrapper">
+															<div class="team-inner" style="background-image: url('assets/img/team/3.jpg')" >
+															<a href="https://www.facebook.com/profile.php?id=100004766234588" target="_blank" > <i class="fa fa-facebook" ></i></a>
+															</div>
+															<div class="description">
+															<h3>Ganesh Sawhney</h3>
+															<h5> <strong> Developer & Designer </strong></h5>
+															<p>
+															An ambitious, hardworking professional targeting challenging and enriching assignments.
+															</p>
+															</div>
+															</div>
+															</div>
+
+														</div>
+														</div>
+														</section>
+														<!--TEAM SECTION END-->
+																		
+
+
+
+
+<!--CONTACT SECTION START-->
+<section id="contact" >
+<div class="container">
+<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+<h3>Contact Details </h3>
+<hr />
+
+</div>
+</div>
+
+<div class="row animate-in" data-anim-type="fade-in-up">
+
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="contact-wrapper">
+<h3>We Are Social</h3>
+<p>
+Talk to us .... We are always there for creative people.
+</p>
+<div class="social-below">
+<a href="https://www.facebook.com/theuntamedpen" class="btn button-custom btn-custom-two" > Facebook</a>
+<!--a href="https://www.facebook.com/theuntamedpen" class="btn button-custom btn-custom-two" > Twitter</a>
+<a href="https://www.facebook.com/theuntamedpen" class="btn button-custom btn-custom-two" > Google +</a-->
+</div>
+</div>
+
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="contact-wrapper">
+<h3>Quick Contact</h3>
+<h4><strong>Email : </strong> info@theuntamedpen.com </h4>
+<h4><strong>Call : </strong> +91 9717694417 </h4>
+<h4><strong>Skype : </strong> ganeshsawhney@yahoo.com </h4>
+</div>
+
+
+</div>
+
+</div>
+
+</div>
+</section>
+<!--CONTACT SECTION END-->
+
+
+
+
+
+<!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME -->
+<!-- CORE JQUERY -->
+<script src="assets/js/jquery-1.11.1.js"></script>
+<!-- BOOTSTRAP SCRIPTS -->
+<script src="assets/js/bootstrap.js"></script>
+<!-- EASING SCROLL SCRIPTS PLUGIN -->
+<script src="assets/js/vegas/jquery.vegas.min.js"></script>
+<!-- VEGAS SLIDESHOW SCRIPTS -->
+<script src="assets/js/jquery.easing.min.js"></script>
+<!-- FANCYBOX PLUGIN -->
+<script src="assets/js/source/jquery.fancybox.js"></script>
+<!-- ISOTOPE SCRIPTS -->
+<script src="assets/js/jquery.isotope.js"></script>
+<!-- VIEWPORT ANIMATION SCRIPTS   -->
+<script src="assets/js/appear.min.js"></script>
+<script src="assets/js/animations.min.js"></script>
+<!-- CUSTOM SCRIPTS -->
+<script src="assets/js/custom.js"></script>
+</body>
+
+</html>
