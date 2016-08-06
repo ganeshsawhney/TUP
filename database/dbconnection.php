f4f3ff65772027c036facd89cee4da7cf74acd4d
Ganesh Sawhney <ganeshsawhney@gmail.com>
2016-08-07 00:15:37 +0530

    Full makeover

--- database/dbconnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $host="localhost";
-  $user="root";
-  $pass="";
-  $db="utp";
-  mysql_connect($host,$user,$pass) or die("database connection failed");
-  mysql_select_db($db) or die("No Database name");
-
-?>
